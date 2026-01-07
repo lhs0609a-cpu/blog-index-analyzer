9d5caa5a0d618893ffd5ec7cb588f207ed01b9cc
lhs0609a-cpu <lhs0609a@naver.com>
2026-01-07 16:30:32 +0900

    fix: Allow extra environment variables in Settings class

--- flyio-backend/config.py
+++ flyio-backend/config.py
@@ -180,6 +180,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
         case_sensitive = False
+        extra = "ignore"  # 정의되지 않은 환경변수 무시
 
 
 @lru_cache()
